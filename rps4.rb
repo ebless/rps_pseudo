dd926f48569aaf30a090955ef73b13bcc29668d9
Ned Bless <ebless@lsoc.org>
2016-01-26 21:00:24 -0600

    Added most necessary code

--- rps4.rb
+++ rps4.rb
@@ -1,22 +1,35 @@
 #set win variable to zero
+win = 0
 #set loses variable to zero
-
+loses = 0
 #Prompt user to chose r, p or s
+
 #get user input, set to choice variable, make sure it's lowercase
 
 #Select computer choice randomly from arrary, set to variable computer
 
 #Three time do iterate
+3.times do
+  puts "Choose r, p, or s"
+  choice = gets.chomp.downcase
+  computer = ['r', 'p', 's'].sample
   #use case statement with [player, computer] (This will be challenging, but totally worth it!)
+  case [choice, computer]
     #when ['p', 'r'], ect..
-      #puts you win
-      # add one to win variable
+    when ['p', 'r'], ['r', 's'], ['s', 'p']
+      puts "You win!"
+      win = win + 1
     #When ['r','r'], ect..
-      #Puts you tied
-    #Else
-      #You Lose
-      # add one to win variable
+    when ['p', 'p'], ['r', 'r'], ['s', 's']
+      puts "You tie!"
+    else
+      puts "You lose!"
+      loses = loses + 1
+    end
+end
 
+puts win
+puts loses
 #Declare Winner at end with score of wins and loses
 
 #### BONUS ####
